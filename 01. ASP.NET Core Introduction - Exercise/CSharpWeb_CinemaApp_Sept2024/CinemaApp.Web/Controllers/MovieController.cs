651e49d716430e647a9ac619f4d32ec4cd0a1fe5
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-09-30 00:46:34 +0300

    Add views

--- 01. ASP.NET Core Introduction - Exercise/CSharpWeb_CinemaApp_Sept2024/CinemaApp.Web/Controllers/MovieController.cs
+++ 01. ASP.NET Core Introduction - Exercise/CSharpWeb_CinemaApp_Sept2024/CinemaApp.Web/Controllers/MovieController.cs
@@ -22,5 +22,47 @@ namespace CinemaApp.Web.Controllers
 
             return View(allMovies);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            // TODO: Add form model + validation
+            this.dbContext.Movies.Add(movie);
+            this.dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Details(string id)
+        {
+            bool isValid = Guid.TryParse(id, out Guid guidId);
+            if (!isValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            Movie? movie = this.dbContext.Movies
+                .FirstOrDefault(x => x.Id == guidId);
+
+            if (movie == null)
+            {
+                //Non-existing movie guid
+                return RedirectToAction(nameof(Index));
+            }
+
+            //if(!this.dbContext.Movies.Any(x => x.Id == guidId))
+            //{
+            //    return RedirectToAction(nameof(Index));
+            //}
+
+            return View(movie);
+        }
     }
 }
