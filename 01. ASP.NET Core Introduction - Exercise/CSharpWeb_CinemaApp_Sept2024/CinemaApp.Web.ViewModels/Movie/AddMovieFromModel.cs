49c3d8526fec18c718cc7c337827b56a9e679377
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-10-01 13:57:09 +0300

    Add validation

---
+++ 01. ASP.NET Core Introduction - Exercise/CSharpWeb_CinemaApp_Sept2024/CinemaApp.Web.ViewModels/Movie/AddMovieFromModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApp.Web.ViewModels.Movie
+{
+    public class AddMovieFromModel
+    {
+        [Required]
+        [MaxLength]
+        public string Title { get; set; } = null!;
+    }
+}
